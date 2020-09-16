ffe088a477c882e9462778cb4825462de745a13a
Daniel Ramirez <43587456+djr52@users.noreply.github.com>
2020-09-16 15:37:38 -0400

    Delete isPhoneNum.py

--- isPhoneNum.py
+++
@@ -1,9 +0,0 @@
-import re
-
-phoneNumReg = re.compile(r'(\d{1,3})?-(\d\d\d-\d\d\d\d)')
-
-match = phoneNumReg.findall('My phone number is 201-270-8615 and 201-675-8535')
-print(match)
-#print(match.groups())
-print(phoneNumReg.search('hello'))
-
